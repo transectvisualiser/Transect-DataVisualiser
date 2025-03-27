fb2712268796c1a2c80131bf0689ff66b584bc39
bac <bl442252@dal.ca>
2025-03-27 01:32:19 -0300

    Made changes in server.py for Deployment

--- server/server.py
+++ server/server.py
@@ -223,6 +223,9 @@ def index():
     return 'Transect Visualizer Backend is running!'
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    import os
+    port = int(os.environ.get('PORT', 5001))  # Fallback to 5001 locally
+    app.run(host='0.0.0.0', port=port)
+
 
 
